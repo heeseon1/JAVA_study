3f1d58c34032e8ea04095f69ca87e011363cf2f1
heeseon1 <heeseon7789@gmail.com>
2024-08-20 17:57:52 +0900

    0820

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//메인메서드 단축어: psvm
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("Hello and welcome!");
+
+    }
+}
+
---
+++ src/Training_package/input.java
@@ -0,0 +1,18 @@
+package Training_package;
+// 패키지1.패키지2 형식으로 되어 있으면 패키지1>패키지2> 경로에 위치하는 걸 의미
+
+
+public class input {
+    public static void main(String[] args) {
+        //출력 메서드 단축어: sout
+        System.out.println(2 + 5);
+        System.out.println("5" + 1);
+        System.out.println(4.5);
+
+        int a = 1;
+        String b = "5";
+        System.out.println("숫자:" + a);
+        System.out.println("문자:" + b);
+        System.out.println(a + b);
+    }
+}
